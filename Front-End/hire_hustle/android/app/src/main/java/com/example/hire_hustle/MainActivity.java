ba706ef50b1857c537ebfc371e573a75b01c37fe
MazenAymann <94933246+MazenAymann@users.noreply.github.com>
2024-03-27 16:53:13 +0200

    Delete Front-End/hire_hustle directory

--- Front-End/hire_hustle/android/app/src/main/java/com/example/hire_hustle/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.hire_hustle;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- Front-End/hire_hustle/lib/navigation_bar/cubit/navigation_state.dart
+++
@@ -1,8 +0,0 @@
-part of 'navigation_cubit.dart';
-
-@immutable
-abstract class NavigationState {}
-
-class NavigationInitial extends NavigationState {}
-
-class NavigationChangePage extends NavigationState {}
